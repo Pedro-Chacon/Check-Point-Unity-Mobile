844859b0906b43f8aca15b28339dbc515eb155e0
Chacon-study <chaconfiap@gmail.com>
2026-03-14 11:48:51 -0300

    New Obstacule
    adicionado novo obstaculo

---
+++ CheckPoint Runner 3D/Assets/Scripts/AlertSystemThird.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class AlertSystemThird : MonoBehaviour
+{
+
+    [Header("Propiedades Obstaculo")]
+    public float obstaculeSpeed;
+
+   //Se quiser aumentar a VELOCIDADE, TEM QUE AUMETAR OS OUTROS FATORES TBM
+   // Da pro player cortar o brick damage e dai desativa o mesh renderer dele
+   // Criar dps um script Manager q aumenta a velocidade de todos por ROUND??
+    [SerializeField] float tempoAtivo;
+    [SerializeField] float tempoDesativo;
+    public GameObject damageBrick;
+    public GameObject Alert1;
+    public GameObject Alert2;
+    public Transform InitialPosition;
+    MeshRenderer meshRendererAlert1;
+    MeshRenderer meshRendererAlert2;
+    MeshRenderer meshRendererBrickDamage;
+    bool canSpawnDamageBrick;
+    Rigidbody rb;
+    void Start()
+    {
+        canSpawnDamageBrick = true;
+        meshRendererAlert1 = Alert1.GetComponent<MeshRenderer>();
+        meshRendererAlert2 = Alert2.GetComponent<MeshRenderer>();
+        meshRendererBrickDamage = damageBrick.GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (canSpawnDamageBrick)
+        {
+            StartCoroutine(SpawnDamageBrick());
+        }
+    }
+
+    IEnumerator SpawnDamageBrick()
+    {
+        transform.position = InitialPosition.transform.position;
+        Vector3 angulosRotacao = new Vector3(0f, 0, Random.Range(0f, 360f));
+
+        canSpawnDamageBrick = false;
+        meshRendererBrickDamage.enabled = false;
+        meshRendererAlert1.enabled = false;
+        meshRendererAlert2.enabled = false;
+
+        transform.position = new Vector3(transform.position.x, Random.Range(5f, 14f), 4.4f);
+        transform.Rotate(angulosRotacao);
+
+        yield return new WaitForSeconds(tempoDesativo);
+
+        rb.linearVelocity = new Vector3(1f * obstaculeSpeed, rb.linearVelocity.y, rb.linearVelocity.z);
+
+        //Código para piscar
+        #region Piscar
+        meshRendererAlert1.enabled = true;
+        meshRendererAlert2.enabled = true;
+        yield return new WaitForSeconds(2f);
+        meshRendererAlert1.enabled = false;
+        meshRendererAlert2.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        meshRendererAlert1.enabled = true;
+        meshRendererAlert2.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        meshRendererAlert1.enabled = false;
+        meshRendererAlert2.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        meshRendererAlert1.enabled = true;
+        meshRendererAlert2.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        meshRendererAlert1.enabled = false;
+        meshRendererAlert2.enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        meshRendererAlert1.enabled = true;
+        meshRendererAlert2.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        meshRendererAlert1.enabled = false;
+        meshRendererAlert2.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        meshRendererAlert1.enabled = true;
+        meshRendererAlert2.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        meshRendererAlert1.enabled = false;
+        meshRendererAlert2.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        meshRendererAlert1.enabled = true;
+        meshRendererAlert2.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        meshRendererAlert1.enabled = false;
+        meshRendererAlert2.enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        #endregion
+
+        meshRendererBrickDamage.enabled = true;
+        meshRendererAlert1.enabled = true;
+        meshRendererAlert2.enabled = true;
+
+
+        yield return new WaitForSeconds(tempoAtivo);
+
+        canSpawnDamageBrick = true;
+
+    }
+
+
+}
